38e4307bbcd83ae9958331036d0a601f0d0fabdb
CoderX-001 <101294286+CoderX-001@users.noreply.github.com>
2023-03-06 11:24:19 -0900

    Update moduleMisc.js

--- frontend/components/moduleMisc.js
+++ frontend/components/moduleMisc.js
@@ -6,14 +6,7 @@ export const isAuthenticated = (redirect) => {
   if (accessToken !== null && refreshToken !== null && id !== null){
     return location.href = redirect
   }
-}
-
-export const notAuthenticated = (redirect) => {
-  const accessToken = localStorage.getItem('accessToken')
-  const refreshToken = localStorage.getItem('refreshToken')
-  const id = localStorage.getItem('userId')
-
-  if (accessToken === null || refreshToken === null || id === null){
-    return location.href = redirect
+  else if (accessToken === null || refreshToken === null || id === null){
+    return location.href = '/pages/login'
   }
 }
