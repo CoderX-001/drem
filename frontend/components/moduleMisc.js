7155577f2567c5eac7011f48e7f9e50ebf180ee8
CoderX-001 <101294286+CoderX-001@users.noreply.github.com>
2023-03-06 13:00:00 -0900

    Update moduleMisc.js

--- frontend/components/moduleMisc.js
+++ frontend/components/moduleMisc.js
@@ -2,11 +2,15 @@ export const isAuthenticated = (redirect) => {
   const accessToken = localStorage.getItem('accessToken')
   const refreshToken = localStorage.getItem('refreshToken')
   const id = localStorage.getItem('userId')
+  
+  const hiddenToken = document.querySelector('.hidden')
 
   if (accessToken !== null && refreshToken !== null && id !== null){
-    return location.href = redirect
+    if (!hiddenToken || hiddenToken.value === null || hiddenToken.value === "" || hiddenToken.value == "undefined") {
+      return location.href = redirect
+    }
   }
   else if (accessToken === null || refreshToken === null || id === null){
-    return location.href = '/pages/login'
+    if (location.href !== "/" || location.href !== "/pages/login" || location.href !== "/pages/signup") return location.href = '/pages/login'
   }
 }
