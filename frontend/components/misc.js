03493fe36c73a1b8023e38ea8da723d6e80d35be
coderX_001 <emmanuelkhila200@gmail.com>
2023-03-07 17:35:13 +0100

    added guide function

--- frontend/components/misc.js
+++ frontend/components/misc.js
@@ -1,3 +1,28 @@
+// VARIABLES
+export const guide = () => {
+  const options = document.querySelectorAll('.options')
+  const guide = document.querySelector('.guide')
+  let activeOption = null;
+  if (options) {
+    activeOption = document.querySelector('.options.active')
+    guide.style.width = activeOption.clientWidth + 'px'
+    for(let i = 0; i < options.length; i++) {
+      options[i].addEventListener('click', () => {
+        if (!options[i].classList.contains('active')) {
+          activeOption = document.querySelector('.options.active')
+          activeOption.classList.remove('active')
+          options[i].classList.add('active')
+          guide.style.width = options[i].clientWidth + 'px'
+          guide.style.left = options[i].offsetLeft + 'px'
+        }
+        else {
+          return false
+        }
+      })
+    }
+  }
+}
+
 export const savePath = async (path) => {
   if (localStorage.getItem('pathName') !== location.pathname) {
     localStorage.setItem('pathName', path)
